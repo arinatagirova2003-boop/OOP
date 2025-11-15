a9a609221e89485c98c0f28ea3b294386e5c8d6f
arinatagirova2003-boop <arina.tagirova.2003@gmail.com>
2025-11-15 10:07:29 +0100

    Update Dish.cpp

--- lab03_dish-nutrition-calculator/Dish.cpp
+++ lab03_dish-nutrition-calculator/Dish.cpp
@@ -7,46 +7,46 @@ using namespace std;
 Dish::Dish(string n, float p, float f, float c, bool isV)
     : name(n), protein(p), fat(f), carbohydrate(c), isVegan(isV) {
     energy = calculateEnergy();
-    cout << "Конструктор зі списком ініціалізації" << endl;
+    cout << "Constructor with initialization list" << endl;
 }
 
 Dish::Dish() : name("Dish1"), protein(1), fat(1), carbohydrate(1), isVegan(false) {
     energy = calculateEnergy();
-    cout << "Конструктор за замовчуванням" << endl;
+    cout << "Default constructor" << endl;
 }
 
 Dish::Dish(string n, bool isV) : name(n), isVegan(isV) {
     do {
-        cout << "Введіть кількість білків: ";
+        cout << "Enter amount of protein: ";
         cin >> protein;
     } while (protein < 0);
 
     do {
-        cout << "Введіть кількість жирів: ";
+        cout << "Enter amount of fat: ";
         cin >> fat;
     } while (fat < 0);
 
     do {
-        cout << "Введіть кількість вуглеводів: ";
+        cout << "Enter amount of carbohydrates: ";
         cin >> carbohydrate;
     } while (carbohydrate < 0);
 
     energy = calculateEnergy();
-    cout << "Конструктор з параметрами" << endl;
+    cout << "Constructor with parameters" << endl;
 }
 
 Dish::~Dish() {
-    cout << "Деструктор викликано для: " << name << endl;
+    cout << "Destructor called for: " << name << endl;
 }
 
 void Dish::displayInfo() {
-    cout << "Назва страви: " << name << endl;
+    cout << "Dish name: " << name << endl;
     cout << fixed << setprecision(2);
-    cout << "Білки: " << protein << " г, "
-        << "Жири: " << fat << " г, "
-        << "Вуглеводи: " << carbohydrate << " г" << endl;
-    cout << "Енергетична цінність: " << energy << " ккал" << endl;
-    cout << "Веганська: " << (isVegan ? "Так" : "Ні") << endl;
+    cout << "Protein: " << protein << " g, "
+         << "Fat: " << fat << " g, "
+         << "Carbohydrates: " << carbohydrate << " g" << endl;
+    cout << "Energy value: " << energy << " kcal" << endl;
+    cout << "Vegan: " << (isVegan ? "Yes" : "No") << endl;
 }
 
 float Dish::calculateEnergy() {
